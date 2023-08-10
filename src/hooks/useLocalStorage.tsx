760a4eecdd8e470dcff84097657371613b506619
Renan Aquino <renan.aquino99@gmail.com>
2023-08-10 17:37:18 -0300

    test: window type check

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -7,7 +7,7 @@ export function useLocalStorage<T>(item: string, initialValue: T){
     const [value, setValue] = useState<T>(initialValue)
 
     useEffect(() => {
-        if (typeof window === 'undefined') return;
+        if (typeof window == 'undefined') return;
         let value = localStorage.getItem(item)
         if(value) setValue(JSON.parse(value))
     }, [window])
