0fea14a4ff7650f9fd3d60bb9893e309c0145849
Renan Aquino <renan.aquino99@gmail.com>
2023-08-10 17:13:34 -0300

    test: rollback

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -10,7 +10,7 @@ export function useLocalStorage<T>(item: string, initialValue: T){
         if (typeof window === 'undefined') return;
         let value = localStorage.getItem(item)
         if(value) setValue(JSON.parse(value))
-    }, [])
+    }, [window])
 
     const updateLocalStorage = (newValue: T) => {
         setValue(newValue);
