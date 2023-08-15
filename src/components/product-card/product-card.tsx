f92e723babadfe1fc64c8ddf9a9fdc6c9a3c8e27
Renan Aquino <renan.aquino99@gmail.com>
2023-08-15 04:05:06 -0300

    fix: added more missing files

---
+++ src/components/product-card/product-card.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { formatPrice } from "@/app/utils/format-price"
+import { useRouter } from "next/navigation"
+import s from './product-card.module.css'
+interface ProductCardProps{
+    id: string,
+    image: string,
+    title: string,
+    price: number
+}
+
+
+export function ProductCard(props: ProductCardProps){
+
+    const price = formatPrice(props.price)
+    const router = useRouter();
+
+    const handleNavigate = () => {
+        router.push("/product?id=" + props.id)
+        console.log("hi");
+    }
+    
+    return(
+        <div className={s.card} onClick={handleNavigate}>
+            <img src={props.image}/>
+            <div>
+                <h3>{props.title}</h3>
+                <div></div>
+                <p>{price}</p>
+            </div>
+        </div>
+    )
+}
