ae48ca6e0519e5c0208c3d78e0477b78fe48b75e
Renan Aquino <renan.aquino99@gmail.com>
2023-08-15 04:01:40 -0300

    fix: added missing files

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,23 +1,15 @@
 'use client'
 
 import { useProducts } from "@/hooks/useProducts"
-import { ProductCard } from "./product-card";
-import { styled } from "styled-components";
-
-const ListContainer = styled.div `
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 256px);
-    gap: 32px;
-    max-width: 100%;
-    margin-top: 32px;
-`
+import { ProductCard } from "../product-card/product-card";
+import s from './product-list.module.css'
 
 export function ProductList(){
     const { data } = useProducts();
     return(
-        <ListContainer>
+        <div className={s.container}>
             {data?.map(product => 
                  <ProductCard key={product.id} id={product.id} title={product.title} price={product.price_in_cents} image={product.image}/> )}
-        </ListContainer>
+        </div>
     )
 }
