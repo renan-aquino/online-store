73bb05d7a9a96f6ccc875d0dd161b6f0737a2bcf
Renan Aquino <renan.aquino99@gmail.com>
2023-11-09 20:26:51 -0300

    feat: added a link to the github repo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,7 +23,7 @@ const Container = styled.div`
 export function Footer(){
     return(
         <Container>
-            <p>This website was built for portfolio purposes and does not portray a real shop.</p>
+            <p>This website was built for portfolio purposes and does not portray a real shop. Check the GitHub repository <a href="https://online-store-blond.vercel.app/" target="_blank">here.</a></p>
             <p>The photos used throughout the page were provided by <a href="https://www.freepik.com/" target="_blank">Freepik</a> and <a href="https://unsplash.com/" target="_blank">Unsplashed</a>.</p>
         </Container>
     )
