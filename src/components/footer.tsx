e18db406fa166abbbc05ccfa20e9e8e7d854b82f
Renan Aquino <renan.aquino99@gmail.com>
2023-11-10 19:46:47 -0300

    fix: updated repo link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,14 @@ import { styled } from "styled-components"
 
 const Container = styled.div`
     background-color: #1c1c1c;
-    height: 140px;
+    padding: 24px;
     color: white;
     width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
     gap: 5px;
 
     a {
@@ -23,7 +24,7 @@ const Container = styled.div`
 export function Footer(){
     return(
         <Container>
-            <p>This website was built for portfolio purposes and does not portray a real shop. Check the GitHub repository <a href="https://online-store-blond.vercel.app/" target="_blank">here.</a></p>
+            <p>This website was built for portfolio purposes and does not portray a real shop. Check the GitHub repository <a href="https://github.com/renan-aquino/online-store" target="_blank">here.</a></p>
             <p>The photos used throughout the page were provided by <a href="https://www.freepik.com/" target="_blank">Freepik</a> and <a href="https://unsplash.com/" target="_blank">Unsplashed</a>.</p>
         </Container>
     )
